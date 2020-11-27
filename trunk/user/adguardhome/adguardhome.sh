4074ea3b4c8acdc188ff79eae45acb546c432016
dldz00 <75023840+dldz00@users.noreply.github.com>
2020-11-27 12:42:18 +0800

    Update adguardhome.sh

--- trunk/user/adguardhome/adguardhome.sh
+++ trunk/user/adguardhome/adguardhome.sh
@@ -130,8 +130,8 @@ fi
 
 dl_adg(){
 logger -t "AdGuardHome" "下载AdGuardHome"
-wget --no-check-certificate -O /tmp/AdGuardHome.tar.gz https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.104.3/AdGuardHome_linux_mipsle_softfloat.tar.gz
-#curl -k -s -o /tmp/AdGuardHome/AdGuardHome --connect-timeout 10 --retry 3 https://github.com/dldz00/rt-n56u/raw/master/trunk/user/adguardhome/AdGuardHome
+#wget --no-check-certificate -O /tmp/AdGuardHome.tar.gz https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.104.3/AdGuardHome_linux_mipsle_softfloat.tar.gz
+curl -k -s -o /tmp/AdGuardHome/AdGuardHome --connect-timeout 10 --retry 3 https://github.com/dldz00/rt-n56u/raw/master/trunk/user/adguardhome/AdGuardHome
 if [ ! -f "/tmp/AdGuardHome/AdGuardHome" ]; then
 logger -t "AdGuardHome" "AdGuardHome下载失败，请检查是否能正常访问github!程序将退出。"
 nvram set adg_enable=0
